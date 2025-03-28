1323d142c650b2b02fabbb4f2d8530f63e66035c
SachaChaussette <sacha.gastinois@gmail.com>
2025-03-28 14:08:37 +0100

    tentative de fix la cam

--- CameraComponent.cpp
+++ CameraComponent.cpp
@@ -1,5 +1,6 @@
 #include "CameraComponent.h"
 #include "Level.h"
+#include "CameraManager.h"
 
 Camera::UCameraComponent::UCameraComponent(AActor* _owner) : UComponent(_owner)
 {
@@ -30,4 +31,23 @@ Camera::UCameraComponent::UCameraComponent(AActor* _owner, const UCameraComponen
 Camera::UCameraComponent::~UCameraComponent()
 {
 	delete view;
+}
+
+void Camera::UCameraComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	owner->GetLevel()->GetCameraManager().Register(this);
+	LOG(Warning, "BeginPlay");
+}
+
+void Camera::UCameraComponent::Tick(const float _deltaTime)
+{
+	Super::Tick(_deltaTime);
+
+	if (!attachmentComponent || !attachmentComponent->GetOwner()) return;
+
+	LOG(Warning, "Tick");
+
+	//SetCenter(attachmentComponent->GetOwner()->GetPosition());
 }
--- PlayerPawn.h
+++ PlayerPawn.h
@@ -4,11 +4,14 @@
 
 class UStaticMeshComponent;
 class UMovementComponent;
+class UCameraComponent;
 
 class APlayerPawn : public APawn
 {
 	UStaticMeshComponent* mesh;
+	UStaticMeshComponent* mesh1;
 	UMovementComponent* movement;
+	Camera::UCameraComponent* camera;
 public:
 	APlayerPawn(Level* _level, const string& _name = "APawn");
 	APlayerPawn(const APlayerPawn& _other);
